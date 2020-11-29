311aecab5a035ba9c3d4388e6527d6c2058b852a
Oliver Harrison <oharrison1998@outlook.com>
2020-11-29 21:56:00 +0000

    Tidied up paths for reset password

--- wpproject/djnews/urls.py
+++ wpproject/djnews/urls.py
@@ -4,19 +4,7 @@ from django.contrib.auth import views as auth_views
 from djnews.views import *
 
 urlpatterns = [
-    path(
-        'accounts/',
-        include("django.contrib.auth.urls")),
-    path(
-        'profile/',
-        login_required(ProfileView.as_view()),
-        name="profile"),
-    path(
-        'change-password/',
-        auth_views.PasswordChangeView.as_view(template_name='djnews/change-password.html'),
-        name="password-change"),
-    path(
-        '',
-        login_required(LandingView.as_view()),
-        name="landing"),
+    path('accounts/', include("django.contrib.auth.urls")),
+    path('profile/', login_required(ProfileView.as_view()), name="profile"),
+    path('', login_required(LandingView.as_view()), name="landing"),
 ]
