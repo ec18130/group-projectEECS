020f6c2ae5d33c3b9d16fe89bb6f33fcb7cc7e66
Oliver Harrison <oharrison1998@outlook.com>
2020-11-29 23:02:06 +0000

    Welcome email set up

--- wpproject/djnews/views.py
+++ wpproject/djnews/views.py
@@ -1,8 +1,12 @@
+from smtplib import SMTPException
+
 from django.contrib.auth import login
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
+
 from djnews.forms import CustomUserCreationForm
 
 
@@ -21,9 +25,23 @@ class RegisterView(View):
             {"form": CustomUserCreationForm}
         )
 
+    # TODO: prevent users with the same email
     def post(self, request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect(reverse("landing"))
+            try:
+                send_mail(
+                    'Welcome!',
+                    'Thank you for joining DJNews!',
+                    'ecs639uprojectsuperuser@gmail.com',
+                    [user.email],
+                    fail_silently=False,
+                )
+            except SMTPException as e:
+                print('Error sending email')
+            finally:
+                return redirect(reverse("landing"))
+        else:
+            return render(request, 'djnews/register.html', {'form': form})
