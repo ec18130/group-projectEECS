661df6c5fc4fb01244f0fa63b985426e995efba1
Oliver Harrison <oharrison1998@outlook.com>
2020-11-29 19:43:05 +0000

    Created template structure and class view structure

--- wpproject/djnews/views.py
+++ wpproject/djnews/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# djnews/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class ProfileView(TemplateView):
+    template_name = "djnews/profile.html"
--- wpproject/wpproject/settings.py
+++ wpproject/wpproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djnews',
 ]
 
 MIDDLEWARE = [
@@ -85,18 +86,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
